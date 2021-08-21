9b82d80fafafe4b48eff00b831469d36421062b9
Hamza Tahmi <63428291+4390c336@users.noreply.github.com>
2021-08-21 22:13:06 +0100

    validate domain / move parsing logic to a func

--- setup_test.go
+++ setup_test.go
@@ -34,6 +34,13 @@ func Test_setup(t *testing.T) {
 			}`,
 			true,
 		},
+		{
+			"not a valid domain",
+			`stopdnsrebind {
+				allow ..example.org.
+			}`,
+			true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
