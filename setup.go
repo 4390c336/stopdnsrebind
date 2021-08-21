e16648983b431f8f9e356e09ee164c15ec601330
Hamza Tahmi <63428291+4390c336@users.noreply.github.com>
2021-08-21 20:42:08 +0100

    add support for configuration

--- setup.go
+++ setup.go
@@ -1,6 +1,8 @@
 package stopdnsrebind
 
 import (
+	"fmt"
+
 	"github.com/coredns/caddy"
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
@@ -9,9 +11,20 @@ import (
 func init() { plugin.Register("stopdnsrebind", setup) }
 
 func setup(c *caddy.Controller) error {
+	allowList := []string{}
 
+	for c.Next() {
+		for c.NextBlock() {
+			if c.Val() != "allow" {
+				return plugin.Error("stopdnsrebind", c.Err("only allow operation is supported"))
+			}
+
+			allowList = append(allowList, c.RemainingArgs()...)
+			fmt.Println("[DEBUG] allowList:", allowList)
+		}
+	}
 	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
-		return Stopdnsrebind{Next: next}
+		return Stopdnsrebind{Next: next, AllowList: allowList}
 	})
 
 	return nil
---
+++ setup_test.go
@@ -0,0 +1,46 @@
+package stopdnsrebind
+
+import (
+	"testing"
+
+	"github.com/coredns/caddy"
+)
+
+func Test_setup(t *testing.T) {
+
+	tests := []struct {
+		name    string
+		config  string
+		wantErr bool
+	}{
+		{
+			"allow internal.example.org",
+			`stopdnsrebind {
+				allow internal.example.org.
+			}`,
+			false,
+		},
+		{
+			"allow multiple",
+			`stopdnsrebind {
+				allow internal.example.org. internal.example.net.
+			}`,
+			false,
+		},
+		{
+			"non supported op",
+			`stopdnsrebind {
+				anything internal.example.org.
+			}`,
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctr := caddy.NewTestController("dns", tt.config)
+			if err := setup(ctr); (err != nil) != tt.wantErr {
+				t.Errorf("setup() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
